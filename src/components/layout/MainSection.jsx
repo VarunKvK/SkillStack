031bf362ef95e2a7b111ccb925a65735465a6e04
VarunKv <93326999+VarunKvK@users.noreply.github.com>
2024-08-19 11:37:28 +0530

    Major changes to the UI to various components-III

--- src/components/layout/MainSection.jsx
+++ src/components/layout/MainSection.jsx
@@ -5,12 +5,13 @@ export function MainSection() {
   const words = ["Visualize", "Illuminate", "Chart", "Showcase", "Transform"];
 
   return (
-    <div className="w-full px-2 h-full flex items-center">
+    <div className="h-screen w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <div className="flex flex-col gap-4 w-full">
         <h1 className="md:text-[6rem] text-[3.5rem] leading-[2rem]">
           <FlipWords words={words} className={"font-semibold"} />
           <br />
-          <span className="leading-[0.2rem]">
+          <span className="leading-[0.2rem] opacity-80">
             <span className="md:text-[3rem] text-[1.8rem] px-4">
               your professional growth
             </span>
@@ -20,7 +21,9 @@ export function MainSection() {
             </span>
           </span>
         </h1>
-        <p className="px-4 text-sm opacity-40">Turn your projects into a powerful skill portfolio</p>
+        <p className="px-4 text-sm opacity-40">
+          Turn your projects into a powerful skill portfolio
+        </p>
       </div>
     </div>
   );
