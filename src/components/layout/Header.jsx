ebb7d7f4babce494de37fd5365092f91be61723a
VarunKv <93326999+VarunKvK@users.noreply.github.com>
2024-08-19 09:56:52 +0530

    Major changes to the UI to various components-II

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -9,7 +9,7 @@ import NavbarMenu from './NavbarMenu'
 export default function Header() {
   const { data: session, status } = useSession()
   return (
-    <header className="px-6 py-4 backdrop-blur-sm">
+    <header className="px-4 py-4 backdrop-blur-sm">
       <nav className="flex justify-between items-center">
         <Link href="/" className="text-black dark:text-white text-2xl font-bold font-anek">SkillStack</Link>
         <NavbarMenu  userData={session} status={status}/>
--- src/components/layout/MainSection.jsx
+++ src/components/layout/MainSection.jsx
@@ -5,16 +5,23 @@ export function MainSection() {
   const words = ["Visualize", "Illuminate", "Chart", "Showcase", "Transform"];
 
   return (
-    <div className="w-full h-full px-4 flex items-center">
-      <h1 className="md:text-[6rem] text-[5rem] leading-[2.5rem]">
-        <FlipWords words={words} className={'font-semibold'}/>
-        <br />
-        <span className="leading-[0.1rem]">
-        <span className="md:text-[3rem] text-[2rem] px-4">your professional growth</span>
-        <br />
-        <span className="md:text-[3rem] text-[2rem] px-4">with skillstack</span>
-        </span>
-      </h1>
+    <div className="w-full px-2 h-full flex items-center">
+      <div className="flex flex-col gap-4 w-full">
+        <h1 className="md:text-[6rem] text-[3.5rem] leading-[2rem]">
+          <FlipWords words={words} className={"font-semibold"} />
+          <br />
+          <span className="leading-[0.2rem]">
+            <span className="md:text-[3rem] text-[1.8rem] px-4">
+              your professional growth
+            </span>
+            <br />
+            <span className="md:text-[3rem] text-[1.8rem] px-4">
+              with skillstack
+            </span>
+          </span>
+        </h1>
+        <p className="px-4 text-sm opacity-40">Turn your projects into a powerful skill portfolio</p>
+      </div>
     </div>
   );
 }
