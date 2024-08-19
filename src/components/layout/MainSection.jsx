b7e42ce124826e5da05a958f04b997b0578d1a46
VarunKv <93326999+VarunKvK@users.noreply.github.com>
2024-08-19 12:42:37 +0530

    Major changes to the UI to various components-IV

--- src/components/layout/MainSection.jsx
+++ src/components/layout/MainSection.jsx
@@ -38,7 +38,7 @@ export function MainSection() {
         <div className="px-4 flex items-center gap-8">
           <Link href={"/"} className="relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
             <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#000_0%,#9afd6c_30%,#d6ff1e_100%)]" />
-            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg dark:bg-black bg-white px-3 py-1 text-sm font-medium dark:text-white text-black backdrop-blur-3xl">
+            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-black px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
               Get Started
             </span>
           </Link>
