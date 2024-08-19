e54f69b225d576304786c14fc510d488f9d4441f
VarunKv <93326999+VarunKvK@users.noreply.github.com>
2024-08-19 14:44:57 +0530

    About-Section Created

--- src/components/layout/FeatureSection.jsx
+++ src/components/layout/FeatureSection.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const FeatureSection = () => {
   return (
-    <div>FeatureSection</div>
+    <div className='h-[40rem] w-full dark:bg-black bg-white relative pt-12'>
+        <div className="max-w-6xl mx-auto flex flex-col gap-4 px-4">
+            <h1 className="md:text-[3rem] text-[2rem]">Unlock the Power of Your Experience</h1>
+        </div>
+    </div>
   )
 }
 
