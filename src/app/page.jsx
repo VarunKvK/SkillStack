f9d273a582c0d7dd817e5b3bc1981075e46ab736
VarunKv <93326999+VarunKvK@users.noreply.github.com>
2024-08-19 12:23:43 +0530

    Major changes to the UI to various components-IV

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import { MainSection } from "@/components/layout/MainSection";
 
 export default function Home() {
   return (
-    <div className="h-screen font-anek bg-[#f1f1f1] dark:bg-[#000]">
+    <div className="h-screen font-anek bg-white dark:bg-[#000]">
       <Header/>
       <MainSection/>
     </div>
--- src/components/layout/MainSection.jsx
+++ src/components/layout/MainSection.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { FlipWords } from "../ui/flip-words";
+import { Button } from "../ui/button";
 
 export function MainSection() {
   const words = ["Visualize", "Illuminate", "Chart", "Showcase", "Transform"];
 
   return (
-    <div className="h-screen w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+    <div className="h-[40rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(circle,transparent_20%,black)]"></div>
       <div className="flex flex-col gap-4 w-full">
-        <h1 className="md:text-[6rem] text-[3.5rem] leading-[2rem]">
+        <h1 className="md:text-[6rem] text-[3.5rem] leading-[2rem] ">
           <FlipWords words={words} className={"font-semibold"} />
           <br />
           <span className="leading-[0.2rem] opacity-80">
-            <span className="md:text-[3rem] text-[1.8rem] px-4">
+            <span className="md:text-[3rem] text-[1.8rem] px-4 ">
               your professional growth
             </span>
             <br />
@@ -24,6 +25,14 @@ export function MainSection() {
         <p className="px-4 text-sm opacity-40">
           Turn your projects into a powerful skill portfolio
         </p>
+        <div className="px-4">
+          <Button className="relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#000_0%,#9afd6c_30%,#d6ff1e_100%)]" />
+            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-black px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+              Get Started
+            </span>
+          </Button>
+        </div>
       </div>
     </div>
   );
